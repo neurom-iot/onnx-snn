e3cbb0bfbe60ffac01000f518fd8791be311869e
SangminPark <psm9733@naver.com>
2019-10-07 05:32:37 +0900

    Add: keras-dnn

---
+++ src/dense_train.py
@@ -0,0 +1,40 @@
+from keras.datasets import mnist
+from keras import models
+from keras import layers
+from keras.utils import to_categorical
+from keras.models import load_model
+from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+# from keras.applications.resnet50 import ResNet50
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+# 이미지 데이터 준비하기 (모델에 맞는 크기로 바꾸고 0과 1사이로 스케일링)
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype('float32') / 255
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype('float32') / 255
+
+
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+
+model = models.Sequential()
+model.add(layers.Dense(512, activation='relu',  e=(28 * 28,)))
+model.add(layers.Dense(10, activation='softmax'))
+
+
+model.compile(optimizer='rmsprop',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+
+# # fit() 메서드로 모델 훈련 시키기
+# model.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+# # 테스트 데이터로 정확도 측정하기
+# test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+# print('test_acc: ', test_acc)
+
+# 모델 저장
+model.save("keras_model.h5")
