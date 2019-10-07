81cb12a16faabceea772a5335430759f3cc9d3c5
SangminPark <psm9733@naver.com>
2019-10-07 20:53:34 +0900

    Modify: op_type convert code modified

--- src/convert_snnOnnx.py
+++ src/convert_snnOnnx.py
@@ -3,17 +3,30 @@ import onnx
 # sn: Spiking Neuron
 
 def convert_snnOnnx(onnx_path):
+    sn_index = 1
+    conv_index = 1
+    maxpool_index = 1
     onnx_model = onnx.load(onnx_path)
-    sn_name = "Sn"
     node_len = len(onnx_model.graph.node)
     for index in range(node_len):
         op_type = onnx_model.graph.node[index].op_type.lower()
         if op_type == "relu" or op_type == "sigmoid" or op_type == "tanh":
-            onnx_model.graph.node[index].name = sn_name
-            onnx_model.graph.node[index].op_type = sn_name
-            onnx_model.graph.node[index].output[0] = onnx_model.graph.node[index].output[0].lower().replace(op_type, sn_name)
-            onnx_model.graph.node[index + 1].input[0] = onnx_model.graph.node[index + 1].input[0].lower().replace(op_type, sn_name)
-    onnx.save(onnx_model, 'ssn.onnx')
+            convert_name = "Sn"
+            onnx_model.graph.node[index].op_type = convert_name
+            onnx_model.graph.node[index].name = convert_name + "_" + str(sn_index)
+            sn_index = sn_index + 1
+        elif op_type == "conv":
+            convert_name = "Sn_Conv2D"
+            onnx_model.graph.node[index].op_type = convert_name
+            onnx_model.graph.node[index].name = convert_name + "_" + str(conv_index)
+            conv_index = conv_index + 1
+        elif op_type == "maxpool":
+            convert_name = "Sn_MaxPool2D"
+            onnx_model.graph.node[index].op_type = convert_name
+            onnx_model.graph.node[index].name = convert_name + "_" + str(maxpool_index)
+            maxpool_index = maxpool_index + 1
+
+    onnx.save(onnx_model, 'snn.onnx')
 
 if __name__ == "__main__":
     convert_snnOnnx('cnnmodel.onnx')
