97d6a547635ff047333754c2e994b57e1ad341c1
SangminPark <psm9733@naver.com>
2019-10-08 03:12:54 +0900

    Merge: convert_snnOnnx.py + onnxToNengo.py

--- src/onnxToNengo.py
+++ src/onnxToNengo.py
@@ -17,16 +17,17 @@ class onnxToNengo:
         self.nengoCode += "import numpy as np\n"
 
         #Network code
+        self.neuron_type = self.getNeuronType()
         self.nengoCode += "with nengo.Network() as net:\n"
         node_len = len(self.onnx_model.graph.node)
         input_info = np.array(self.onnx_model.graph.input[0].type.tensor_type.shape.dim)
         for index in range(node_len):
             node_info = self.onnx_model.graph.node[index]
             op_type = node_info.op_type.lower()
-            if op_type == "sn_conv2d":
+            if op_type == "conv":
                 self.nengoCode += self.genConv(node_info)
 
-            elif op_type == "sn_maxpool2d":
+            elif op_type == "maxpool":
                 self.nengoCode += self.genMaxpool(node_info)
 
             elif op_type == "reshape":
@@ -42,7 +43,6 @@ class onnxToNengo:
 
     def genConv(self, node_info):
         input_shape = self.getInputShape(node_info)
-        print(input_shape)
         for index in range(len(node_info.attribute)):
             if node_info.attribute[index].name == "kernel_shape":
                 kernel_shape = np.array(node_info.attribute[index].ints)
@@ -69,7 +69,6 @@ class onnxToNengo:
 
     def genMatmul(self, node_info):
         input_shape = self.getInputShape(node_info)
-        print(input_shape)
         code = ""
         code += "matmul\n"
         return code
@@ -77,6 +76,15 @@ class onnxToNengo:
     def genSoftmax(self, node_info):
         code = ""
         code += "softmax\n"
+        return code
+    
+    def getNeuronType(self):
+        node_len = len(self.onnx_model.graph.node)
+        for index in range(node_len):
+            node_info = self.onnx_model.graph.node[index]
+            op_type = node_info.op_type.lower()
+            if op_type == "lif":
+                code = "LIF"
         return code
 
     def makefile(self, result_path):
@@ -93,14 +101,33 @@ class onnxToNengo:
             weight_name = self.onnx_model.graph.initializer[m].name
             for n in range(len(node_info.input)):
                 if node_info.input[n] == weight_name and regex.findall(weight_name):
-                    print(weight_name)
                     shape = self.onnx_model.graph.initializer[m].dims
                     return shape
         return None
 
     def printNode(self):
         print(self.onnx_model.graph.node)
+
+class convert_snnOnnx:
+    def __init__(self):
+        return
+
+    def convert_snnOnnx(self, onnx_path, result_path, sn_kind):
+        sn_index = 1
+        onnx_model = onnx.load(onnx_path)
+        node_len = len(onnx_model.graph.node)
+        for index in range(node_len):
+            op_type = onnx_model.graph.node[index].op_type.lower()
+            if op_type == "relu" or op_type == "sigmoid" or op_type == "tanh":
+                onnx_model.graph.node[index].op_type = sn_kind
+                onnx_model.graph.node[index].name = sn_kind + "_" + str(sn_index)
+                sn_index = sn_index + 1
+        onnx.save(onnx_model, result_path)
         
 if __name__ == "__main__":
-    otn = onnxToNengo("snn.onnx")
+    onnx_file_path = "dnn2onnx.onnx"
+    result_file_path = "dnn2snn.onnx"
+    cso = convert_snnOnnx()
+    cso.convert_snnOnnx(onnx_file_path, result_file_path, "LIF")
+    otn = onnxToNengo(result_file_path)
     otn.makefile("nengo_code.py")
