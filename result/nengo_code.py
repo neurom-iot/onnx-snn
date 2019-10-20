83fb09bf25dfb002608d128f08d6d7cf128a1e0d
SangminPark <psm9733@naver.com>
2019-10-21 06:01:31 +0900

    Modify: genConv2dLayer, genMax2dLayer function

--- result/nengo_code.py
+++ result/nengo_code.py
@@ -1,9 +1,19 @@
-import nengo
+#onnx to nengo convert codeimport nengo
+import nengo_dl
 import numpy as np
+
 with nengo.Network() as net:
-conv
-conv
-maxpool
+	net.config[nengo.Ensemble].max_rates = nengo.dists.Choice([100])
+	net.config[nengo.Ensemble].intercepts = nengo.dists.Choice([0])
+	neuron_type = nengo.LIF(amplitude=0.01)
+	nengo_dl.configure_settings(trainable=False)
+	
+	inp = nengo.Node([0] * 28 * 1)
+	x = nengo_dl.tensor_layer(inp, tf.layers.conv2d, shape_in=(28, 28, 1), filters=32, kernel_size=3)
+	x = nengo_dl.tensor_layer(x, neuron_type)
+	x = nengo_dl.tensor_layer(inp, tf.layers.conv2d, shape_in=(26, 26, 32), filters=64, kernel_size=3)
+	x = nengo_dl.tensor_layer(x, neuron_type)
+	x = nengo_dl.tensor_layer(x, tf.layers.average_pooling2d, shape_in=(24, 24, 64), pool_size=2, strides=2)
 flatten
 matmul
 matmul
--- src/onnxToNengo.py
+++ src/onnxToNengo.py
@@ -13,79 +13,92 @@ class onnxToNengo:
 
     def setNetwork(self):
         #init code generating
-        self.nengoCode += self.genInit(self.neuron_type)
+        amplitude = 0.01
+        self.nengoCode += self.genInit(self.neuron_type, amplitude)
 
         #layer code generating
         onnx_model_graph = self.onnx_model.graph
         onnx_model_graph_node = onnx_model_graph.node
         node_len = len(onnx_model_graph.node)
-        input_info = np.array(onnx_model_graph.input[0].type.tensor_type.shape.dim)
-        self.nengoCode += self.genInput(input_info)
+        input_shape = np.array(onnx_model_graph.input[0].type.tensor_type.shape.dim)
+        temp = []
+        regex = re.compile(":.*\d*")
+        for index in range(1, len(input_shape)):
+            data = regex.findall(str(input_shape[index]))[0]
+            temp.append(int(data[2:len(data)]))
+        input_shape = temp
+        code, output_shape = self.genInputLayer(input_shape)
+        self.nengoCode += code
 
         for index in range(node_len):
             node_info = onnx_model_graph_node[index]
             op_type = node_info.op_type.lower()
             if op_type == "conv":
-                self.nengoCode += self.genConv(index, onnx_model_graph_node)
-
+                code, output_shape = self.genConv2dLayer(output_shape, index, onnx_model_graph_node)
+                self.nengoCode += code
             elif op_type == "maxpool":
-                self.nengoCode += self.genMaxpool(node_info)
-
+                code, output_shape = self.genMaxpool2dLayer(output_shape, node_info)
+                self.nengoCode += code
             elif op_type == "reshape":
                 regex = re.compile("flatten")
                 if regex.findall(node_info.name):
-                    self.nengoCode += self.genFlatten(node_info)
-
+                    code = self.genFlatten(node_info)
+                    self.nengoCode += code
             elif op_type == "matmul":
-                self.nengoCode += self.genMatmul(index, onnx_model_graph_node)
+                code = self.genMatmul(index, onnx_model_graph_node)
+                self.nengoCode += code
 
-    def genInit(self, neuron_type):
-        code = ""
+    def genInit(self, neuron_type, amplitude):
+        code = "#onnx to nengo convert code"
         code += "import nengo\n"
+        code += "import nengo_dl\n"
         code += "import numpy as np\n\n"
         code += "with nengo.Network() as net:\n"
         code += "\tnet.config[nengo.Ensemble].max_rates = nengo.dists.Choice([100])\n"
         code += "\tnet.config[nengo.Ensemble].intercepts = nengo.dists.Choice([0])\n"
-        code += "\tneuron_type = nengo." + str(neuron_type) + "(amplitude=0.01)\n"
+        code += "\tneuron_type = nengo." + str(neuron_type) + "(amplitude=" + str(amplitude) + ")\n"
         code += "\tnengo_dl.configure_settings(trainable=False)\n"
         code += "\t\n"
         return code
     
-    def genInput(self, input_info):
-        length = len(input_info)
+    def genInputLayer(self, input_shape):
+        length = len(input_shape)
         dim_array = []
         for index in range(1, length):
-            dim_array.append(re.findall('\d+', str(input_info[index]))[0])
-        
+            dim_array.append(re.findall('\d+', str(input_shape[index]))[0])
         code = ""
         code += "\tinp = nengo.Node([0]"
         for index in range(len(dim_array)):
             code += " * " + str(dim_array[index])
         code += ")\n"
-        return code
+        output_shape = input_shape
+        return code, output_shape
 
-    def genConv(self, index, onnx_model_graph_node):
+    def genConv2dLayer(self, input_shape, index, onnx_model_graph_node):
         node_info = onnx_model_graph_node[index]
         neuron_type = self.getNeuronType(index, onnx_model_graph_node)
-        input_info = self.getLayer2LayerInputDataInfo(node_info)
+        filters = self.getFilterSize(node_info)
         for index in range(len(node_info.attribute)):
             if node_info.attribute[index].name == "kernel_shape":
-                kernel_shape = np.array(node_info.attribute[index].ints)
+                kernel_size = node_info.attribute[index].ints[0]
             elif node_info.attribute[index].name == "strides":
-                stride = node_info.attribute[index].ints
+                strides = node_info.attribute[index].ints[0]            #conv2d stride: default 1
+        output_shape = [int((input_shape[0] - kernel_size) / strides + 1), int((input_shape[1] - kernel_size) / strides + 1), filters]
         code = ""
-        code += "conv\n"
-        return code
+        code += "\tx = nengo_dl.tensor_layer(inp, tf.layers.conv2d, shape_in=(" + str(input_shape[0]) + ", " + str(input_shape[1]) + ", " + str(input_shape[2]) + "), filters=" + str(filters) + ", kernel_size=" + str(kernel_size) + ")\n"
+        code += "\tx = nengo_dl.tensor_layer(x, neuron_type)\n"
+        return code, output_shape
 
-    def genMaxpool(self, node_info):
+    def genMaxpool2dLayer(self, input_shape, node_info):
         for index in range(len(node_info.attribute)):
             if node_info.attribute[index].name == "kernel_shape":
-                kernel_shape = np.array(node_info.attribute[index].ints)
+                pool_size = node_info.attribute[index].ints[0]
             elif node_info.attribute[index].name == "strides":
-                stride = node_info.attribute[index].ints
+                strides = node_info.attribute[index].ints[0]            #poolv2d stride: default 2
+        output_shape = [int(input_shape[0]/strides), int(input_shape[1]/strides), input_shape[2]]
         code = ""
-        code += "maxpool\n"
-        return code
+        code += "\tx = nengo_dl.tensor_layer(x, tf.layers.average_pooling2d, shape_in=(" + str(input_shape[0]) + ", " + str(input_shape[1]) + ", " + str(input_shape[2]) + "), pool_size=" + str(pool_size) + ", strides=" + str(strides) + ")\n"
+        return code, output_shape
 
     def genFlatten(self, node_info):
         code = ""
@@ -95,7 +108,6 @@ class onnxToNengo:
     def genMatmul(self, index, onnx_model_graph_node):
         node_info = onnx_model_graph_node[index]
         neuron_type = self.getNeuronType(index, onnx_model_graph_node)
-        input_info = self.getLayer2LayerInputDataInfo(node_info)
         code = ""
         code += "matmul\n"
         return code
@@ -121,14 +133,14 @@ class onnxToNengo:
     def getNengoCode(self):
         return self.nengoCode
 
-    def getLayer2LayerInputDataInfo(self, node_info):
+    def getFilterSize(self, node_info):
         regex = re.compile("W|W\d*")
         for m in range(len(self.onnx_model.graph.initializer)):
             weight_name = self.onnx_model.graph.initializer[m].name
             for n in range(len(node_info.input)):
                 if node_info.input[n] == weight_name and regex.findall(weight_name):
                     shape = self.onnx_model.graph.initializer[m].dims
-                    return shape
+                    return shape[0]
         return None
 
     def printNode(self):
