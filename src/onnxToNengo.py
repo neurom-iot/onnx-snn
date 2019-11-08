4f60d5e6fff9120e11d5f70c4555b25cdb0fabd7
SangminPark <psm9733@naver.com>
2019-11-08 23:41:25 +0900

    Update onnxToNengo.py

--- src/onnxToNengo.py
+++ src/onnxToNengo.py
@@ -290,7 +290,7 @@ class convert_snnOnnx:
     def __init__(self):
         return
 
-    def convert_snnOnnx(self, onnx_path, result_path, neuron_type):
+    def run(self, onnx_path, result_path, neuron_type):
         MODEL_SAVE_FOLDER_PATH = '../model/onnx2snn/'
 
         if not os.path.exists(MODEL_SAVE_FOLDER_PATH):
@@ -304,12 +304,12 @@ class convert_snnOnnx:
                 onnx_model.graph.node[index].op_type = neuron_type
                 onnx_model.graph.node[index].name = neuron_type + "_" + str(sn_index)
                 sn_index = sn_index + 1
-        onnx.save(onnx_model, MODEL_SAVE_FOLDER_PATH + result_path)
+        onnx.save(onnx_model, result_path)
         
 if __name__ == "__main__":
     onnx_file_path = "../model/model2onnx/vgg162onnx.onnx"
     result_file_path = "../model/onnx2snn/vgg162snn.onnx"
     cso = convert_snnOnnx()
-    cso.convert_snnOnnx(onnx_file_path, result_file_path, "LIF")
+    cso.run(onnx_file_path, result_file_path, "LIF")
     otn = onnxToNengoCode(result_file_path)
     otn.makefile("../result/nengo_code.py")
